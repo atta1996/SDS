210f237380e745352f15f351414465705283634d
Alejandro Villapalos <alejandrovillapalos@gmail.com>
2020-05-22 17:42:16 +0200

    Creado acceso a cliente y servidor desde 'main'
    Pendiente de revisión

--- UI/main.go
+++ UI/main.go
@@ -5,84 +5,30 @@ package main
 
 import (
 	"fmt"
-	"io/ioutil"
-	"log"
-	"net/url"
 	"os"
-	"os/signal"
-	"runtime"
-	"sync"
-
-	"github.com/zserge/lorca"
 )
 
 // Go types that are bound to the UI must be thread-safe, because each binding
 // is executed in its own goroutine. In this simple case we may use atomic
 // operations, but for more complex cases one should use proper synchronization.
-type counter struct {
-	sync.Mutex
-	count int
-	ui    lorca.UI
-}
-
-func (c *counter) Redirect(archivo string) {
-
-	c.Lock()
-	defer c.Unlock()
-
-	// Load HTML.
-	b, err := ioutil.ReadFile("./www/" + archivo + ".html") // just pass the file name
-	if err != nil {
-		fmt.Print(err)
-	}
-	html := string(b) // convert content to a 'string'
-	c.ui.Load("data:text/html," + url.PathEscape(html))
-}
 
 func main() {
-	args := []string{}
-	if runtime.GOOS == "linux" {
-		args = append(args, "--class=Lorca")
+
+	fmt.Println("login.go :: un ejemplo de login mediante TLS/HTTP en Go.")
+	s := "Introduce srv para funcionalidad de servidor y cli para funcionalidad de cliente"
+
+	if len(os.Args) > 1 {
+		switch os.Args[1] {
+		case "srv":
+			fmt.Println("Entrando en modo servidor...")
+			server()
+		case "cli":
+			fmt.Println("Entrando en modo cliente...")
+			client()
+		default:
+			fmt.Println("Parámetro '", os.Args[1], "' desconocido. ", s)
+		}
+	} else {
+		fmt.Println(s)
 	}
-	ui, err := lorca.New("", "", 550, 605, args...)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer ui.Close()
-
-	// A simple way to know when UI is ready (uses body.onload event in JS)
-	ui.Bind("start", func() {
-		log.Println("UI is ready")
-	})
-
-	// Create and bind Go object to the UI
-	c := &counter{}
-	c.ui = ui
-
-	c.ui.Bind("redirect", c.Redirect)
-
-	// Load HTML.
-	b, err := ioutil.ReadFile("./www/login.html") // just pass the file name
-	if err != nil {
-		fmt.Print(err)
-	}
-	html := string(b) // convert content to a 'string'
-	ui.Load("data:text/html," + url.PathEscape(html))
-
-	// You may use console.log to debug your JS code, it will be printed via
-	// log.Println(). Also exceptions are printed in a similar manner.
-	ui.Eval(`
-		console.log("Hello, world!");
-		console.log('Multiple values:', [1, false, {"x":5}]);
-	`)
-
-	// Wait until the interrupt signal arrives or browser window is closed
-	sigc := make(chan os.Signal)
-	signal.Notify(sigc, os.Interrupt)
-	select {
-	case <-sigc:
-	case <-ui.Done():
-	}
-
-	log.Println("exiting...")
 }
